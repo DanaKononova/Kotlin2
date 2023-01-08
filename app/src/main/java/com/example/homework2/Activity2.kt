28502e3e5f3b7f0af55d451a2c254b15c2c9afd7
DanaKononova <dana.kononova@mail.ru>
2023-01-08 21:40:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/homework2/Activity2.kt
@@ -0,0 +1,19 @@
+package com.example.homework2
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class Activity2 : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId", "SetTextI18n")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_2)
+        val count = intent.getIntExtra("some_count", 0)
+        val textView = findViewById<TextView>(R.id.textView2)
+        textView.text = textView.text.toString()+count.toString();
+        textView.setOnClickListener {
+        }
+    }
+}
---
+++ app/src/main/java/com/example/homework2/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.homework2
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var count = 0;
+        val randomIntent = Intent (this, Activity2::class.java)
+        val textView = findViewById<TextView>(R.id.textView)
+        textView.setOnClickListener {
+            textView.text = textView.text.subSequence(0,9).toString()+count.toString();
+            randomIntent.putExtra("some_count", count)
+            count++;
+        }
+        val button1 = findViewById<Button>(R.id.button1)
+        button1.setOnClickListener {
+            startActivity(randomIntent)
+        }
+    }
+}
